b15949f32f3dc6573955f8a852339e3969a16f85
Kidus Mammo <kidus.mammo@aalto.fi>
2020-05-04 19:17:46 +0300

    quotation error

--- file_operations/step3.sh
+++ file_operations/step3.sh
@@ -3,7 +3,7 @@ STRING='Hello World'
 
 output_text() {
 
-  if [[ "${1}" == "cat ${FILENAME}" ]] ; then
+  if [[ ${1} == "cat ${FILENAME}" ]] ; then
   	    echo -e "\\033[8m<done>3</done>\\033[0m"; 
   fi
 }
